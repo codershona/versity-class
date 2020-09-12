95e1a8928deba702267706072dae99b76b8b962c
Coder--Shona <falgunimst95@gmail.com>
2020-09-12 12:09:05 +0000

    multiple choice done

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
- print 'Hello, World!'
+#  print 'Hello, World!'
 
 
-  1/2
-  type(1/2)
-  print(01)
-  1/(2/3) 
+#   1/2
+#   type(1/2)
+#   print(01)
+#   1/(2/3) 
+
+def f1():
+
+    return "ab"
+
+ def f2():
+
+    return f1() * 2
+
+ def f3():
+
+    return f2() + f1() * 3
+
+ print(f3()
