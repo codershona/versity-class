6033003589105cac7fc704ce3bdbeaa47a6b82c0
Coder--Shona <falgunimst95@gmail.com>
2020-09-12 16:25:24 +0000

    discussin forum assignment is almost done

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-#  print 'Hello, World!'
-
-
-#   1/2
-#   type(1/2)
-#   print(01)
-#   1/(2/3) 
-
-def f1():
-
-    return "ab"
-
- def f2():
-
-    return f1() * 2
-
- def f3():
-
-    return f2() + f1() * 3
-
- print(f3()
+def d():  
+    print(a)  
+  
+# Global scope 
+a = "I am a CS student"
+d()
