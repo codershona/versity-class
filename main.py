1d076bd2844d13d4a2b82ffab6142a3e5cd78594
Coder--Shona <falgunimst95@gmail.com>
2020-09-13 15:26:27 +0000

    programming assignments done

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-import math
+def new_line(): # this is testable 
+  print()
 
-def n1(radius):
-    nb =  4 * math.pi * radius * radius
-    vol = (4 / 17) * math.pi * radius * radius * radius
-    print("\n This is the surface area of a sphere = %.5f" %nb)
-    print("\n This is the volume of a sphere = %.5f" %vol)
+def three_lines(): # defining 3 lines
+  new_lines()
+  new_lines()
+  new_lines()
 
-n1(6)
+def nine_lines(): # defining 9 line
+  three_lines()
+  three_lines()
+  three_lines()
+
+print("first line has been explained.") 
+print("its time for total nine blank lines....!")
+print("second line also been described.")
+
+def clear_screen(): #trying to print 25 line clear_screen
+  nine_lines()
+  nine_lines()
+  three_lines()
+  three_lines()
+  new_line()
+print("line one ")
+print("here is twenty-five blank lines has been added...!")
+# clear_screen()
+print("this is last line")
