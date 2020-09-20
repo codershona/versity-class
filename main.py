af25c06aec79717a11a2f26da1e1253b07bb462c
Coder--Shona <falgunimst95@gmail.com>
2020-09-20 17:22:47 +0000

    finish learning journal 2

--- main.py
+++ main.py
@@ -1,30 +1,11 @@
-import sys
-
-def count_down(x):
-  if x <= 1:
-    print("BLAST OFF!")
-  else:
-    print(x)
-    count_down(x-1)
-
-def count_up(y):
-  if y >= 2:
-    print("BLAST OFF!")
-  else:
-    print(y)
-    count_up(y+1)
-
-if sys.version_info[0] == 3:
-  number = input('Please write down number here ')
-else:
-  number = input('Please write down number here ')
-3
-number = int(number)
-
-if number > 2:
-  count_down(number)
-elif number < 4:
-  count_up(number)
-else:
-  # in case of 0, both functions will print 'Blastoff!'
-  print("BLAST OFF!")
+try: 
+   country_code = int(country_code) 
+   print("+02") 
+except: 
+   print("Please check again before writing3!!!!") 
+try: 
+    case1 = input("Write your country code number: ") 
+    case2 = int(case1) 
+    print("Your country number has been added successfully!!!!", case2) 
+except ValueError: 
+    print("OOPs! please write your country code????") 
