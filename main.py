1474057418d846b97819a5dc2c0a389316b0607e
Coder--Shona <falgunimst95@gmail.com>
2020-09-20 16:51:12 +0000

    learning journal ans 1 done

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-def case_one(x1, y2):
-    if len(x1) > 3:
-        if len(y2) > 3:
-            print(f"{x1} are measure to {y2}")
-        else:
-            print(f"{x1} are not measure to {y2}")
-    else:
-        print('The measurement might not be exact! Sorry!')
+import sys
 
+def count_down(x):
+  if x <= 1:
+    print("BLAST OFF!")
+  else:
+    print(x)
+    count_down(x-1)
 
-def case_two(a1, b2):
-    if len(a1) > 3 and len(b2) > 3:
-        print(f"{a1} output would be {b2}")
-    else:
-        print(f"{a1} output not would be {b2}")
+def count_up(y):
+  if y >= 2:
+    print("BLAST OFF!")
+  else:
+    print(y)
+    count_up(y+1)
 
-case_one('Lima', 'Anny')
-case_one('Robin', 'Rohit')
-print('----!!!!!!!---This is the case one output----!!!!\n')
+if sys.version_info[0] == 3:
+  number = input('Please write down number here ')
+else:
+  number = input('Please write down number here ')
+3
+number = int(number)
 
-case_two('Lisa', 'Bobby')
-case_two('Pinky', 'Brown')
-
-print('----!!!!!!!---This is the case two output----!!!!\n')
+if number > 2:
+  count_down(number)
+elif number < 4:
+  count_up(number)
+else:
+  # in case of 0, both functions will print 'Blastoff!'
+  print("BLAST OFF!")
