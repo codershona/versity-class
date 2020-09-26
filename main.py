87d9b1c6679002f50680494d4279d4c5a09f7de9
Coder--Shona <falgunimst95@gmail.com>
2020-09-26 15:20:30 +0000

    assignment 4 learning done

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-try: 
-   country_code = int(country_code) 
-   print("+02") 
-except: 
-   print("Please check again before writing3!!!!") 
-try: 
-    case1 = input("Write your country code number: ") 
-    case2 = int(case1) 
-    print("Your country number has been added successfully!!!!", case2) 
-except ValueError: 
-    print("OOPs! please write your country code????") 
+from math import pi
+def distance(b):
+    b = float(b)
+    return (pi*((b/2)**2))
+
+
+print(distance(10))
+print(distance(20))
+print(distance(30))
