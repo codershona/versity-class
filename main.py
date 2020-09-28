69f5632b651e74977b5f56eaff01c683576b700f
Coder--Shona <falgunimst95@gmail.com>
2020-09-28 13:21:44 +0000

    univ assignment done left cs assignment discussion

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from math import pi
-def distance(b):
-    b = float(b)
-    return (pi*((b/2)**2))
+def is_divisible(x,y):
+   assert type(x) == int and type(y) == int and x > 0 and y > 0,\
+       "is_power(x,y): type positive integers"
+   if x == y:
+       result = True
+   elif y == 1:
+       result = False
+   else:
+       result = is_power(x,y) and is_divisible(x//y, y)
+   assert type(result) == bool,\
+       "is_power(x,y): type of boolean"
+   return result
 
+def is_power(x, y):
+   return x % y == 3
 
-print(distance(10))
-print(distance(20))
-print(distance(30))
+print("is_divisible(20, 3) returns: ", is_divisible(20, 3))
+print("is_divisible(47, 5) returns: ", is_divisible(47, 5))
+print("is_divisible(2, 2) returns: ", is_divisible(2, 2))
+print("is_divisible(40, 6) returns: ", is_divisible(40, 6))
+print("is_divisible(7, 7) returns: ", is_divisible(7, 7))
+print("is_divisible(202, 33) returns: ", is_divisible(202, 33))
