6a00509f1b17053df5adbd82aa7223a49ac10c9f
Coder--Shona <falgunimst95@gmail.com>
2020-10-04 10:26:27 +0000

    programming assignment 5 done

--- main.py
+++ main.py
@@ -1,40 +1,21 @@
 import math
 
-def my_sqrt(a):
-    x = a/2
-    while True:
-        est_root = (x + a/x) / 2.0
-        if est_root == x:
-            return est_root
-            break
-        x = est_root
+def square_root(x):
+  tol = 0.00001
+  est = 0.1
 
-def square_root(types_of_a):
-    list1a = "a"
-    list1b = "my_sqrt(a)"
-    list1c = "math.sqrt(a)"
-    list1d = "diff"
-    
-    list2a = "-"
-    list2b = "---------"
-    list2c = "------------"
-    list2d = "----"
-    
-    space1 = " "
-    space2 = " " * 3
-    space3 = ""
-    
-    print(list1a, space1, list1b, space2, list1c, space3, list1d)
-    print(list2a, space1, list2b, space2, list2c, space3, list2d)
-    
-    for a in types_of_a:
-        column1 = float(a)
-        column2 = my_sqrt(a)
-        column3 = math.sqrt(a)
-        column4 = abs(my_sqrt(a) - math.sqrt(a))
+  while True:
+    est= (est + float(x) / est) / 2.0
+    differ = abs(float(x) - est ** 2.0)
+    if differ <= tol:
+      break
 
-        print(column1, "{:<13f}".format(column2), "{:<13f}".format(column3), column4)
-square_root(range(1,12))
-square_root((1,5))
-square_root((1,3))
-square_root(range(1,9))
+
+  diff = abs(math.sqrt(float(x))- est)
+  print("a = ",x, " | square_root(a) = ", est, " | math.sqrt(a) = ", math.sqrt(float(x)), " | diff = ", diff)
+
+x = 1
+while(x<=25):
+  square_root(x)
+  x+=1
+
