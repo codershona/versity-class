e6a5ca10be934485f37718e9df17e3b3a594c6bd
Coder--Shona <falgunimst95@gmail.com>
2020-10-11 16:44:27 +0000

    learning journal assignment 6 part 1 done

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-def any_lowercase3(s):
-     for c in s:
-          flag = c.islower()
-     return flag
+lists_string = 'apple orange grape pomegranate blueberry blackcherry strawberry banana pear avacado custardapple apricot almond peanuts cashew chestnut ambrella argan'
+
+'''Adding a string to item list'''
+items_list = lists_string.split()
+
+'''Removing orange from items lists'''
+items_list.remove('orange')
+items_list.pop(-1)
+del items_list[1]
+
+'''Accessing the sort items list'''
+items_list.sort()
+
+'''Creating and adding those items elements'''
+items_list.append('atemoya')
+items_list.insert(5,'babaco')
+items_list.extend(['bacuri', 'dates'])
+
+'''Listing to String'''
+access_string = ' '.join(items_list)
+print(access_string)
