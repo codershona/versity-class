190014b79d9e1fa66a8d3c41c9d6cdc7c51a4b55
Coder--Shona <falgunimst95@gmail.com>
2020-10-13 05:48:24 +0000

    finish final draft

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-'''Nesting the list'''
-numbers_of_list = [1,2,3,4,[5,6],7]
+def add_value(x):
+  x.append(7)
+  y = [1, 0]
 
-'''*this is a operator elements'''
-thislist = ['pineapple','jackfruit','cherry']*3
-
-'''Listing the slices'''
-this_slice = thislist[4:6]
-
-'''this is a += operator method'''
-thislist += ['grapes', 'banana']
-
-'''Listing the filter'''
-boolean = [True, False, False, True]
-list(filter(None,boolean))
-x = [4, 5, 7, 6, 12, 14]
-x = x.append(27)
-print(x)
+  add_value(y)
+  print(y)
