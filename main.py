4b57e13e4a2145863bc54e6ded8372938de1fbad
Coder--Shona <falgunimst95@gmail.com>
2020-10-17 16:22:32 +0000

    discussion 7 point 1 done

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
-list_shellter = {
-  "Tuples_1": ["list",4,"item"],
-  "Tuples_2": ["list",1,"item"],
-  "Tuples_3": ["list",5,"item2"],
-  "Tuples_4": ["unlist",3,"item"],
-  "Tuples_5": ["sublist",10,"item"],
-  "Tuples_6": ["sublist",8,"item"],
-}
+d={'List':'Item1','Item2':'Item3','Item4':'Item5'} 
+Z = list(d.items())
+print('d.items() = ',Z)
+print('Differences of d.items() value is ',type(Z[1]))
+for j,w in d.items(): 
+    print('The keys are ', j, ', the values are ',w)
 
-
-print(list_shellter)
-print("")
-
-
-def invert_dict(d):
-  inverse = dict()
-  for key in d:
-    val = d[key]
-    for item in val:
-      if item not in inverse:
-        inverse[item] = [key]
-      else:
-        inverse[item].append(key)
-  return inverse 
-
-
-inverted_tup = invert_dict(list_shellter)
-print(inverted_tup)
+#example of dictinary
+#list of key,value pairs, which are tuples
+#loop over the dictionary using key,value tuples
