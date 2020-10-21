045cb06a6cae30ada1639093ece93498cebe2a5a
Coder--Shona <falgunimst95@gmail.com>
2020-10-21 08:44:13 +0000

    answer 1 done

--- main.py
+++ main.py
@@ -1,41 +1,16 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-test_dups = ["zzz","dog","bookkeeper","subdermatoglyphic","subdermatoglyphics"]
-test_miss = ["zzz","subdermatoglyphic","the quick brown fox jumps over the lazy dog"]
+alphabet = 'abcdefghijklmnopqrstuvwxyz'    
+test_miss = ["cat","xyz"] 
+def dup_letters(s):
+    missWords = ""
+    global alphabet
+    i = 0
+    while i < len(alphabet):
+        if alphabet[i] not in s:
+            missWords += alphabet[i]
+        i += 1
+    sortedlists = sorted(missWords)
+    sortedAlphabets = ''.join(sortedlists)
+    return sortedAlphabets
 
-def histogram(s):
-    d = dict()
-    for c in s:
-        if c not in d:
-            d[c] = 1
-        else:
-            d[c] += 1
-    return d
-
-def has_duplicates(s):
-    for j in histogram(s).values():
-        if j > 2:
-            return True
-    return False
-
-def test_dups_loop():
-    for x in test_dups:
-        print(x + ' -', has_duplicates(x))
-
-def miss_letters(s):
-    y = list('abcdefghijklmnopqrstuvwxyz')
-    s = s.lower()
-    for w in s.lower():
-        if w in y:
-            y.remove(w)
-    return ''.join(y)
-
-def test_miss_loop():
-    for w in test_miss:
-        print(w + ' =', miss_letters(w))
-
-def main():
-    test_dups_loop()
-    test_miss_loop()
-
-if __name__ == '__main__':
-    main()
+for i in test_miss:
+    print('{} are skipping alphabets lists {}'.format(i, dup_letters(i)))
