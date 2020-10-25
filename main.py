4727ded62abd7da5a1f314c362051e16f68fcc2b
Coder--Shona <falgunimst95@gmail.com>
2020-10-25 17:24:15 +0000

    complete all assessment

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-alphabet = 'abcdefghijklmnopqrstuvwxyz'    
-test_miss = ["cat","xyz"] 
-def dup_letters(s):
-    missWords = ""
-    global alphabet
-    i = 0
-    while i < len(alphabet):
-        if alphabet[i] not in s:
-            missWords += alphabet[i]
-        i += 1
-    sortedlists = sorted(missWords)
-    sortedAlphabets = ''.join(sortedlists)
-    return sortedAlphabets
-
-for i in test_miss:
-    print('{} are skipping alphabets lists {}'.format(i, dup_letters(i)))
+try:
+ X = 23
+ X = X + ' 3 '
+except TypeError:
+ print('type error')
